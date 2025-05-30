97f16967d574eaf07c03e09ed84574c89396585a
whizkidanime <whizkidanime@gmail.com>
2025-05-30 17:42:29 +0300

    Исправлена конфигурация для GitHub User Pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/wishlist-checker/',
+  base: '/',
   build: {
     rollupOptions: {
       input: './index.html'
